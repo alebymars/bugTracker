e4cf4b46814512352ad7be4721a6ccf5933817fb
Snollyg0ster <32766884+Snollyg0ster@users.noreply.github.com>
2023-03-09 23:19:14 +0700

    Test new hook

--- client/src/hoc/RequireAuth.tsx
+++ client/src/hoc/RequireAuth.tsx
@@ -7,22 +7,28 @@ interface Props {
 }
 
 const RequireAuth = (props: Props) => {
+    const restored = isRestored();
     const location = useLocation();
     // const token = useSelector(state => state.user.token);
     const user = useSelector(state => state.user);
-    const localStorageUser = localStorage.getItem("user");
-    console.log("localStorageUser => ", localStorageUser);
+//     const localStorageUser = localStorage.getItem("user");
+//     console.log("localStorageUser => ", localStorageUser);
 
     // console.log("token => ", token);
     console.log("user => ", user.isAuth);
+    
+    if (!restored) {
+        // здесь может возвращаться компонент загрузки
+        return;
+    }
 
-    // if (!user.isAuth) {
-    //     return <Navigate to={`/login?redirectTo=${location.pathname}`} state={{from: location}}/>;
-    // }
-    // временное решение, пока Ваня не сделает новый хук в своей библиотеке
-    if (!localStorageUser) {
+    if (!user.isAuth) {
         return <Navigate to={`/login?redirectTo=${location.pathname}`} state={{from: location}}/>;
     }
+    // временное решение, пока Ваня не сделает новый хук в своей библиотеке
+//     if (!localStorageUser) {
+//         return <Navigate to={`/login?redirectTo=${location.pathname}`} state={{from: location}}/>;
+//     }
 
     return props.children;
 };
